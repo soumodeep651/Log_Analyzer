983d3ca5c50fbede2ead00dc4b7a185719a0d1ea
Soumo <ssoumodeep651@gmail.com>
2026-08-10 14:11:44 +0530

    Top 3 Errors in Logs

--- Top_three_logs.py
+++ Top_three_logs.py
@@ -31,14 +31,22 @@ def analyze_log(file_path):
         "error_summary": count
     }
 
-
 file_path = "production.log"  # Replace with the actual path to your log file
-analyze_log_result = analyze_log(file_path)
-print("Total lines:", analyze_log_result["total_lines"])
-print("Errors:", analyze_log_result["errors"])
+analyzed_log = analyze_log(file_path)
+error_summary = analyzed_log["error_summary"]
+error_list = list(error_summary.items())
+
+for i in range(0, len(error_list)):
+    for j in range(0, len(error_list)- i -1):
+       if error_list[j][1] < error_list[j+1][1]:
+           error_list[j], error_list[j+1] = error_list[j+1], error_list[j]
+        
+
+print("Total lines:", analyzed_log["total_lines"])
+print("Errors:", analyzed_log["errors"])
 print("Error summary:")
 
-for message, count in analyze_log_result["error_summary"].items():
+for message, count in error_list[:3]:
     print(f"  {message}: {count}")
-
+ 
 
